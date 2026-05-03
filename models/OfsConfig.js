74b011a0f45336803ade2f4aa0a604d7693250c7
daniherrera <u20201e786@upc.edu.pe>
2026-05-03 00:09:49 -0500

    feat: enable full editing of OFS 'Who we are' and footer from admin panel

--- models/OfsConfig.js
+++ models/OfsConfig.js
@@ -13,6 +13,22 @@ const OfsConfigSchema = new mongoose.Schema({
         type: String,
         default: 'Convento San Antonio de Padua, Chiclayo, Perú'
     },
+    quienesSomos: {
+        type: String,
+        default: 'Caminamos junto a nuestros hermanos mayores de la Orden Franciscana Seglar, quienes nos acompañan y guían en nuestro camino de fe y servicio.'
+    },
+    footerDireccion: {
+        type: String,
+        default: 'Convento San Antonio de Padua, Chiclayo, Perú'
+    },
+    footerEmail: {
+        type: String,
+        default: 'jufrapomalca@gmail.com'
+    },
+    footerTelefono: {
+        type: String,
+        default: '+51 979 948 528'
+    },
     updatedAt: {
         type: Date,
         default: Date.now
