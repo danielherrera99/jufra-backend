50e13a10db28f658f4560543469f529a1bd9609a
daniherrera <u20201e786@upc.edu.pe>
2026-08-28 21:52:35 -0500

    Actualizar horarios del cron job para extraer métricas

--- jobs/cronJobs.js
+++ jobs/cronJobs.js
@@ -4,8 +4,8 @@ const MetricaSocial = require('../models/MetricaSocial');
 
 // Programar la tarea para que se ejecute todos los días a las 3:00 AM
 // El formato del cron es: 'minuto hora dia_del_mes mes dia_de_la_semana'
-// '0 3,10,18 * * *' = todos los días a las 03:00 AM, 10:00 AM y 06:00 PM
-const cronSchedule = '0 3,10,18 * * *';
+// 0 minutos, a las 2:00, 8:00, 14:00 y 20:00
+const cronSchedule = '0 2,8,14,20 * * *';
 
 function startCronJobs() {
     console.log(`[Cron] Tarea automática programada: Recopilación de métricas (Hora: ${cronSchedule})`);
